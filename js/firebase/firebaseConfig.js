0bf8334d3f1975e82dd289c53e2442546e740f31
Alimenur <alimenur.a.z@gmail.com>
2026-03-14 18:05:53 +0300

    Firebase konfigürasyonu güncellendi

--- js/firebase/firebaseConfig.js
+++ js/firebase/firebaseConfig.js
@@ -1,1 +1,9 @@
 //firebase bağlantısı
+const firebaseConfig = {
+  apiKey: "AIzaSyDlnH1bN4dUo6qMLaQRHdY1TmwZhhCFCJE",
+  authDomain: "satir-arasi-5ed31.firebaseapp.com",
+  projectId: "satir-arasi-5ed31",
+  storageBucket: "satir-arasi-5ed31.firebasestorage.app",
+  messagingSenderId: "573269163098",
+  appId: "1:573269163098:web:d388698dc4708a3b7b6c63"
+};
